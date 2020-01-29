113ee0d5a00c00e1d2d21b12ce9ce453fca47c0a
patsys <ba9f11ecc3497d9993b933fdc2bd61e5@patweber.de>
2020-01-29 11:14:41 +0100

    add debug information

--- security/security.go
+++ security/security.go
@@ -18,7 +18,7 @@ func BasicAuth(w http.ResponseWriter, r *http.Request) bool {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(401)
 		w.Write([]byte("{ \"message\" : \"Unauthorised.\" }"))
-		log.Println("Request is not correctly authorised.")
+		log.Println("Request is not correctly authorised. Should be " + username + ":" + password + " is " + user + ":" password )
 		return false
 	}
 	log.Println("Request is correctly authorised.")
